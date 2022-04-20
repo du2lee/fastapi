1d0f85bb6211f8c048fbb40d8695f9c9eb3718d3
duhui <dh151446@gmail.com>
2022-04-20 10:58:28 +0900

    test commit

--- FastAPI/database/mongoDB.py
+++ FastAPI/database/mongoDB.py
@@ -17,7 +17,7 @@ db = MongoDataBase()
 
 def getConnection() -> AsyncIOMotorClient:
     # tdd 시 connectMongo()가 시작되지 않아 아래 2줄 추가
-    mongoAddress = os.getenv('MONGO_ADDRESS')
+    mongoAddress = 'mongodb://interx:interx%40504@server.interxlab.io:15115/admin'
     db.client = AsyncIOMotorClient(mongoAddress)
     return db.client
 
